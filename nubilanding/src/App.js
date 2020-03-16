d0bd86e14ed8b89337445dee3504da78579c1d6a
jenn-lang <jennifervallejos08@gmail.com>
2020-03-16 18:47:31 -0300

    agregue texto y algunos estilos solo para diferenciar los espacios, importe los fonts y agregue e instale sass

--- nubilanding/src/App.js
+++ nubilanding/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Beneficios from './componentes/beneficios/beneficios';
 
 function App() {
   return (
     <div className="App">
-
+      <Beneficios/>
     </div>
   );
 }
---
+++ nubilanding/src/componentes/beneficios/beneficios.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import styles from './styles.module.scss'
+ 
+class Beneficios extends React.Component{
+    render(){
+        return(
+            <div className= {styles.contenedor}>
+                <div className={styles.Contenedor}>
+                    <h2 className={styles.titulo}>Rápido.</h2>
+                    <p className={styles.guion}>-</p>
+                    <p className={styles.texto}>Realizá tu recarga o retiro de tu cuenta PayPal con solo unos clicks.</p>
+                </div>
+                <div className={styles.Contenedor}>
+                    <h2 className={styles.titulo}>Seguro.</h2>
+                    <p className={styles.guion}>-</p>
+                    <p className={styles.texto}>Contamos con monitoreo de fraudes 24/7 y cubrimos 100% de las transacciones no autorizadas verificadas.</p>
+                </div>
+                <div className={styles.Contenedor}>
+                    <h2 className={styles.titulo}>Simple.</h2>
+                    <p className={styles.guion}>-</p>
+                    <p className={styles.texto}>Olvidate de los papeleos inagotables. Con Nubi hacé todo en 4 simples pasos desde donde quieras.</p>
+                </div>
+                <div className={styles.Contenedor}>
+                    <h2 className={styles.titulo}>Transparente.</h2>
+                    <p className={styles.guion}>-</p>
+                    <p className={styles.texto}>Conocé dónde está tu dinero en todo momento.
+                       <br></br> Sin comisiones ocultas.</p>
+                </div>
+                <botton>Registrate Gratis</botton>
+            </div>
+        )
+    }
+}
+ 
+export default Beneficios;
