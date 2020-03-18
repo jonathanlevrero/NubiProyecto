47128e9d88583ee968eac1cdc88a8a66aaea5ecd
MatiiAutalan <autalan.matiash@gmail.com>
2020-03-17 21:59:32 -0300

    agregue logos de las diferentes tiendas

--- nubilanding/src/components/recargas/recargas.js
+++ nubilanding/src/components/recargas/recargas.js
@@ -1,5 +1,14 @@
-import React from 'react'
-import styles from '../recargas/styles.module.scss'
+import React from 'react';
+import styles from '../recargas/styles.module.scss';
+import LogoPlay from '../../assets/img_logo_playstation.png';
+import LogoForever from '../../assets/img_logo_forever21.png';
+import LogoTwitch from '../../assets/img_logo_twitch.png';
+import LogoTripAdvisor from '../../assets/img_logo_tripadvisor.png';
+import LogoAirbnb from '../../assets/img_logo_airbnb.png';
+import LogoEbay from '../../assets/img_logo_ebay.png';
+import LogoTiendaMia from '../../assets/img_logo_tiendamia.png';
+import LogoCarters from '../../assets/img_logo_carters.png';
+import LogoOrigin from '../../assets/img_logo_origin.png';
 
 class Recargas extends React.Component {
     render () {
@@ -17,6 +26,20 @@ class Recargas extends React.Component {
                 </div>
                 <div className={styles.contenedorAhorra}>
                     <h2 className={styles.titulos}>Ahorrá en tus compras online</h2>
+                    <div className={styles.contenedorLogosPF}>
+                        <img className={styles.logosF} src= {LogoPlay} alt="Logo Playstation Store"/>
+                        <img className={styles.logos} src= {LogoForever} alt="Logo Forever 21"/>
+                        <img className={styles.logos} src= {LogoTwitch} alt="Logo Twitch"/>
+                        <img className={styles.logos} src= {LogoTripAdvisor} alt="Logo Trip Advisor"/>
+                        
+                    </div>
+                    <div className={styles.contenedorLogosSF}>
+                        <img className={styles.logosF} src= {LogoAirbnb} alt="Logo Airbnb"/>  
+                        <img className={styles.logos} src= {LogoEbay} alt="Logo Ebay"/>
+                        <img className={styles.logos} src= {LogoTiendaMia} alt="Logo Tienda Mia"/>
+                        <img className={styles.logos} src= {LogoCarters} alt="Logo Carter's"/>
+                        <img className={styles.logos} src= {LogoOrigin} alt="Logo Origin"/>
+                    </div>
                     
 
                 </div>
