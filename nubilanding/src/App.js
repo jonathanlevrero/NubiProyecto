2b406fe9889937a77ecb0f97af59cc3f3452c276
MatiiAutalan <autalan.matiash@gmail.com>
2020-03-19 13:56:49 -0300

    seccion Retiros terminada

--- nubilanding/src/App.js
+++ nubilanding/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Recargas from './components/recargas/recargas'
+import Retiros from './components/retiros/retiros'
 
 function App() {
   return (
     <div>
       <Recargas />
+      <Retiros />
     </div>
   );
 }
