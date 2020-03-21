9e4308e042b156ec1a0d4855ee050ff3d635c750
MatiiAutalan <autalan.matiash@gmail.com>
2020-03-21 19:06:19 -0300

    cree seccion footer, falta acomodar logos de nubi y paypal

--- nubilanding/src/App.js
+++ nubilanding/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import Footer from './components/footer/footer';
 
 function App() {
   return (
     <div className="App">
-
+      <Footer />
     </div>
   );
 }
