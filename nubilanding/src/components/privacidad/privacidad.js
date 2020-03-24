8bc300318a3ba725e7f5407edd8c092f6c46f870
MatiiAutalan <autalan.matiash@gmail.com>
2020-03-24 16:31:02 -0300

    agregue titulo con imagen de fondo

---
+++ nubilanding/src/components/privacidad/privacidad.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from '../privacidad/styles.module.scss';
+
+class Privacidad extends React.Component {
+    render () {
+        return (
+            <div className= {styles.contenedor}>
+                <div className= {styles.contenedorTitulo}>
+                    <h1>Política de privacidad Nubi</h1>
+                </div>
+                <div className= {styles.contenedorTituloSecundario}>
+                    <p>
+                        Nubi respeta tu privacidad y por eso te contamos que hacemos con la 
+                        información que compartís con nosotros ahora.
+                    </p>
+                </div>
+
+            </div>
+        )
+    }
+}
+
+export default Privacidad ;
