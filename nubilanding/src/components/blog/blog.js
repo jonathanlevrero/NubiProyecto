643609f1863cb9a87e82308b2057edd6ed2be9d3
jenn-lang <jennifervallejos08@gmail.com>
2020-03-26 13:42:13 -0300

    modifique el archivo blog.js quite todo de la tags de imagenes y las puse en estilos, agregue mas contenido en estilos,para el fondo y los botones

--- nubilanding/src/components/blog/blog.js
+++ nubilanding/src/components/blog/blog.js
@@ -1,26 +1,22 @@
 import React from 'react';
 import styles from './styles.module.scss';
-import Escritorio from '../../assets/bg_blog.png';
-import Icono from '../../assets/ic_send2_blue.svg';
+
 class Blog extends React.Component{
     render(){
         return(
             <div className={styles.contenedorGeneral}>
-                <h1 className={styles.titulo}>¡Sumate a nuestro Blog!</h1>
-                {/*<div className={styles.contenedor}>*/}
-                {/*pienso poner contenedores */}
-                <p className={styles.texto}>Enterate de todas las novedades de Nubi<br></br> suscribiendote a nuestro Blog</p>
-                <form className={styles.form}>
-        {/*<label>dejanos tu email</label>*/}  
-                    <input className={styles.input}></input>
-                    <botton className={styles.enviar}>si</botton>
-                </form>
-                <botton className={styles.ver}>Ver blog</botton>
-            <img  className={styles.Escritorio} src= {Escritorio} alt="escritorio"/>
-            <img  className={styles.Icono} src= {Icono} alt="escritorio"/>
-                {/*</div>*/}
+                <div className={styles.contenedor}>
+                    <h1 className={styles.titulo}>¡Sumate a nuestro Blog!</h1>
+                    <p className={styles.texto}>Enterate de todas las novedades de Nubi<br></br> suscribiendote a nuestro Blog</p>
+                    <form  className={styles.form}>
+                        <input type="email"   value="Dejanos tu email" className={styles.input}></input>
+                        <botton type="botton" className={styles.enviar}></botton>
+                    </form><br></br>
+                    <botton className={styles.ver}>Ver blog</botton>
+                    
+                </div>
             </div>
-        );
+        )
     }
 }
 
