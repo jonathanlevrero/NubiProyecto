c1db9ddb948c55ddafe207ebc8102d3289ecad6b
MatiiAutalan <autalan.matiash@gmail.com>
2020-03-27 15:31:22 -0300

    Acomodando codigo

--- nubilanding/src/components/footer/footer.js
+++ nubilanding/src/components/footer/footer.js
@@ -1,45 +1,43 @@
-import React from 'react';
-import styles from '../footer/styles.module.scss'
-import LogoNubi from '../../assets/img_logo_nubi_white.svg'
-import LogoPaypal from '../../assets/secured_paypal.png'
-import LogoInstagram from '../../assets/ic_footer_instagram.svg'
-import LogoTwitter from '../../assets/ic_footer_twitter.svg'
-import LogoFacebook from '../../assets/ic_footer_facebook.svg'
-import LogoYoutube from '../../assets/ic_footer_youtube.svg'
+import React from "react";
+import styles from "../footer/styles.module.scss";
+import LogoNubi from "../../assets/img_logo_nubi_white.svg";
+import LogoPaypal from "../../assets/secured_paypal.png";
+import LogoInstagram from "../../assets/ic_footer_instagram.svg";
+import LogoTwitter from "../../assets/ic_footer_twitter.svg";
+import LogoFacebook from "../../assets/ic_footer_facebook.svg";
+import LogoYoutube from "../../assets/ic_footer_youtube.svg";
 
-
-
-class Footer extends React.Component {
-    render () {
-        return (
-            <div className= {styles.contenedor}>
-                <div className= {styles.contenedorLogoNubi}>
-                    <img src={LogoNubi} alt='Logo Nubi'/>
-                    <p>Copyright © 2017. <b>Nubi</b> S.A</p>
-                </div>
-                <div className={styles.contenedorLogoPaypal}>
-                    <img src={LogoPaypal} alt='Logo Paypal'/>
-                </div>
-                <div className= {styles.seguinos}>
-                    <h2>Seguinos!</h2>
-                    <div className={styles.logosRedes}>
-                    <img src={LogoInstagram} alt='Logo Instagram'/>
-                    <img src={LogoTwitter} alt='Logo Twitter' />
-                    <img src={LogoFacebook} alt='Logo Facebook' />
-                    <img src={LogoYoutube} alt='Logo Youtube'/>
-                    </div>
-                </div>
-                <div className={styles.contenedorBotones}>
-                    <button>Ayuda</button>
-                    <button>Términos y condiciones</button>
-                    <button>Nosotros</button>
-                    <button>Privacidad</button>
-                    <button>Nuestro blog</button>
-                    <button>Prensa</button>
-                </div>
-            </div>
-        )
-    }
-}
+function Footer() {
+  return (
+    <div className={styles.contenedor}>
+      <div className={styles.contenedorLogoNubi}>
+        <img src={LogoNubi} alt="Logo Nubi" />
+        <p>
+          Copyright © 2017. <b>Nubi</b> S.A
+        </p>
+      </div>
+      <div className={styles.contenedorLogoPaypal}>
+        <img src={LogoPaypal} alt="Logo Paypal" />
+      </div>
+      <div className={styles.seguinos}>
+        <h2>Seguinos!</h2>
+        <div className={styles.logosRedes}>
+          <img src={LogoInstagram} alt="Logo Instagram" />
+          <img src={LogoTwitter} alt="Logo Twitter" />
+          <img src={LogoFacebook} alt="Logo Facebook" />
+          <img src={LogoYoutube} alt="Logo Youtube" />
+        </div>
+      </div>
+      <div className={styles.contenedorBotones}>
+        <button>Ayuda</button>
+        <button>Términos y condiciones</button>
+        <button>Nosotros</button>
+        <button>Privacidad</button>
+        <button>Nuestro blog</button>
+        <button>Prensa</button>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
