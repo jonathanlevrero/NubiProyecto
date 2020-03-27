292f3a0387004b6c0dab82f65cf046e270068bee
jonathanlevrero <jonathan.levrero@gmail.com>
2020-03-27 16:37:03 -0300

    acomodando el codigo

--- nubilanding/src/App.js
+++ nubilanding/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import Home from './components/home/home'
+import Home from './components/home/home';
+
 function App() {
   return (
     <div className="App">
