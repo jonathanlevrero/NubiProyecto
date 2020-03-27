69ca4c52a5f9c819a2fab44cb41486c1678929c4
jonathanlevrero <jonathan.levrero@gmail.com>
2020-03-27 16:03:11 -0300

    estructura basica

---
+++ nubilanding/src/components/Header/Header.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import styles from './styles.module.scss';
+
+
+
+function Header() {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <a>Registrate Gratis</a>
+                    <a>Iniciar Sesión</a>
+                    
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+export default Header;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
