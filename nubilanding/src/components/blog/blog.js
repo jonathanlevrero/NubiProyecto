31fbed9ab59a13bb43983334d2da562b42212ceb
jenn-lang <jennifervallejos08@gmail.com>
2020-03-27 12:47:45 -0300

    en la carpeta blog cree una new file llamada form,js donde pase las cosas de formulario y agregue estilos de margen y padding

--- nubilanding/src/components/blog/blog.js
+++ nubilanding/src/components/blog/blog.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './styles.module.scss';
-
+import Form from './form';
 class Blog extends React.Component{
     render(){
         return(
@@ -8,11 +8,8 @@ class Blog extends React.Component{
                 <div className={styles.contenedor}>
                     <h1 className={styles.titulo}>¡Sumate a nuestro Blog!</h1>
                     <p className={styles.texto}>Enterate de todas las novedades de Nubi<br></br> suscribiendote a nuestro Blog</p>
-                    <form  className={styles.form}>
-                        <input type="email" name="email"  value="Dejanos tu email" className={styles.input}></input>
-                        <botton type="botton" className={styles.enviar}></botton>
-                    </form><br></br>
-                    <botton className={styles.ver}>Ver blog</botton>
+                    <Form />
+                     <botton className={styles.ver}>Ver blog</botton>
                     
                 </div>
             </div>
---
+++ nubilanding/src/components/blog/form.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './styles.module.scss';
+
+
+class Form extends React.Component{
+    render(){
+        return(
+            <form   className={styles.formContenedor}>
+                        {/*<label htmlFor="email">Dejanos tu email</label>*/}
+                        <input type="email" className={styles.input}></input>
+                        <botton type="submit" className={styles.enviar}></botton>
+            </form>
+        )
+    }
+}
+
+
+
+
+
+
+
+export default Form;
