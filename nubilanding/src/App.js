86f52cd45c20daf4b119da38916e6d4dfcb50401
MatiiAutalan <autalan.matiash@gmail.com>
2020-03-27 15:04:03 -0300

    cambie la clase por una funcion y acomode el codigo quitando espacios de mas y agregando ;donde correspondia

--- nubilanding/src/App.js
+++ nubilanding/src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import './App.css';
-import Registrarse from './components/registrarse/registrarse'
+import React from "react";
+import "./App.css";
+import Registrarse from "./components/registrarse/registrarse";
 
 function App() {
   return (
--- nubilanding/src/components/registrarse/registrarse.js
+++ nubilanding/src/components/registrarse/registrarse.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import styles from '../registrarse/styles.module.scss'
+import React from "react";
+import styles from "../registrarse/styles.module.scss";
 
-class Registrarse extends React.Component {
-    render() {
-        return (
-            <div className= {styles.contenedorGeneral}>
-                <h1 className={styles.titulo}>Tu cuenta Nubi en sólo 2 minutos</h1>
-                <button className={styles.boton}>Registrate Gratis</button>
-            </div>
-        );
-    };
-};
+function Registrarse() {
+  return (
+    <div className={styles.contenedorGeneral}>
+      <h1 className={styles.titulo}>Tu cuenta Nubi en sólo 2 minutos</h1>
+      <button className={styles.boton}>Registrate Gratis</button>
+    </div>
+  );
+}
 
 export default Registrarse;
