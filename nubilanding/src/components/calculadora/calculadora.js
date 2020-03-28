696cef9a850dd2284637b0c9ef3f7345f81f3848
MatiiAutalan <autalan.matiash@gmail.com>
2020-03-28 19:19:53 -0300

    agregue formulario con estilos, falta estilizar los input radio

--- nubilanding/src/components/calculadora/calculadora.js
+++ nubilanding/src/components/calculadora/calculadora.js
@@ -1,42 +1,39 @@
-import React from 'react';
-import styles from '../calculadora/styles.module.scss';
-import Flecha from '../../assets/ic_calculator_arrow.svg'
+import React from "react";
+import styles from "../calculadora/styles.module.scss";
+import Flecha from "../../assets/ic_calculator_arrow.svg";
 
 class Calculadora extends React.Component {
-    render () {
-        return(
-            <div className= {styles.contenedor}>
-                <div>
-                    <h2 className= {styles.titulo}>Calculá tu retiro o recarga</h2>
-                </div> 
-                
-                <div className= {styles.calculadora}>
-                    <div className= {styles.seccion}>
-                        <input type='text' value= '0'/>
-                        <div className= {styles.selectores}>
-                            <p>Indicá el monto dólares</p>
-                            <input type="radio" id="recarga" value="recarga"/>
-                            <label htmlFor="recarga">Recarga</label>
-                            <input type="radio" id="retiro" name="gender" value="retiro"/>
-                            <label htmlFor="retiro">Retiro</label>
-                        </div>
-                    </div> 
-                    <div className= {styles.flecha}>
-                        <img src= {Flecha} />
-                    </div>
-                    <div className= {styles.seccion}>
-                        <input type='text' value= '0'/>
-                        <div className= {styles.selectores}></div>
-                            <p>Monto estimado (Tarifa incluída)</p>
-                            <input type="radio" id="pesos" value="pesos"/>
-                            <label htmlFor="pesos">ARS</label>
-                            <input type="radio" id="dolares" name="gender" value="dolares"/>
-                            <label htmlFor="dolares">U$S</label>
-                        </div>
-                    </div>  
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className={styles.contenedor}>
+        <div>
+          <h2 className={styles.titulo}>Calculá tu retiro o recarga</h2>
+        </div>
+        <form className={styles.calculadora}>
+          <div className= {styles.seccionValor}>
+            <input type='text' value='0' name='valor' id='valor' />
+            <label>Indicá el monto en Dólares</label>
+            <input type='radio' id='recarga' name='tipooperacion' value='recarga' />
+            <label htmlFor='recarga'>RECARGA</label>
+            <input type='radio' id='retiro' name='tipooperacion' value='retiro' />
+            <label htmlFor='retiro'>RETIRO</label>
+          </div>
+          <div className={styles.flecha}>
+          <img src={Flecha} alt='Boton para Calcular' />
+          </div>
+          <div className={styles.seccionValor}> 
+            <input type='text' value='0' name='resultado' id='resultado' />
+            <label>Monto estimado (Tarifa incluída)</label>
+            <input type='radio' name='tipomoneda' value='Pesos' />
+            <label>ARS</label>
+            <input type='radio' name='tipomoneda' value='Dolares' />
+            <label>U$S</label>
+          </div> 
+        </form>
+        <button className={styles.boton}>Registrate Gratis</button>
+      </div>
+    );
+  }
 }
 
 export default Calculadora;
