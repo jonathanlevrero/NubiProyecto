3a12bc6802353cf1e46d7f60f17d1abf05d00842
jenn-lang <jennifervallejos08@gmail.com>
2020-03-30 14:21:50 -0300

    cambie los nombres de los contenedores que estaban mal en beneficios.js

--- nubilanding/src/components/beneficios/beneficios.js
+++ nubilanding/src/components/beneficios/beneficios.js
@@ -10,21 +10,21 @@ function Beneficios() {
           <p className={styles.guion}>-</p>
           <p className={styles.texto}>Realizá tu recarga o retiro de tu cuenta PayPal con solo unos clicks.</p>
         </div>
-        <div className={styles.subContenedor}>
+        <div className={styles.contenedorParrafos}>
           <h2 className={styles.titulo}>Simple.</h2>
           <p className={styles.guion}>-</p>
           <p className={styles.texto}>Olvidate de los papeleos inagotables. Con Nubi hacé todo en 4 simples pasos desde donde quieras.</p>
         </div>
-        <div className={styles.subContenedor}>
+        <div className={styles.contenedorParrafos}>
           <h2 className={styles.titulo}>Seguro.</h2>
           <p className={styles.guion}>-</p>
           <p className={styles.texto}>Contamos con monitoreo de fraudes 24/7 y cubrimos 100% de las transacciones no autorizadas verificadas.</p>
         </div>
-        <div className={styles.subContenedor}>
+        <div className={styles.contenedorParrafos}>
           <h2 className={styles.titulo}>Transparente.</h2>
           <p className={styles.guion}>-</p>
           <p className={styles.texto}>Conocé dónde está tu dinero en todo momento.
-                        <br></br> Sin comisiones ocultas.</p>
+            <br></br> Sin comisiones ocultas.</p>
         </div>
       </div>{/* contenedorBeneficios */}
       <botton className={styles.boton}>Registrate Gratis</botton>
