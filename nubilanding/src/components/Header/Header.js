41c9b10c09dedc84e7aec6875bf958d446d7682c
jonathanlevrero <jonathan.levrero@gmail.com>
2020-03-30 15:38:38 -0300

    header terminado

--- nubilanding/src/components/Header/Header.js
+++ nubilanding/src/components/Header/Header.js
@@ -10,7 +10,10 @@ function Header() {
                 <ul>
                     <button className={styles.Registrate}>Registrate Gratis</button>
                     <button className={styles.Iniciarsesion}>Iniciar Sesión</button>
-                    
+                    <button className={styles.Navicon}>
+                        &#9776;
+                    </button>
+
                 </ul>
             </nav>
         </header>
