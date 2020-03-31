94f53916a9c8e5cb2f783f9cf56907b24089c17b
jenn-lang <jennifervallejos08@gmail.com>
2020-03-31 19:57:52 -0300

    maquete mas cosas para el formulario como usar constructor, onChange, acomodar los espacios sean 2, tambien modifique en estilos el input los lados sean mas largos y el boton lo agrande un poco la altura

--- nubilanding/src/components/blog/form.js
+++ nubilanding/src/components/blog/form.js
@@ -2,22 +2,35 @@ import React from 'react';
 import styles from './styles.module.scss';
 
 
-class Form extends React.Component{
-    render(){
-        return(
-            <form   className={styles.formContenedor}>
-                        {/*<label htmlFor="email">Dejanos tu email</label>*/}
-                        <input type="email" placeholder="Dejanos tu email" className={styles.input}></input>
-                        <botton type="submit" className={styles.enviar}></botton>
-            </form>
-        )
+class Form extends React.Component {
+  state = { email: '', boton: '' };
+  constructor(args) {
+    super(args)
+    this.state = {
+
     }
+  }
+
+  onChange (e){
+    this.setState ({
+      [e.target.email]: e.target.value
+    })
+  }
+  render() {
+    return (
+      <form className={styles.formContenedor}>
+        {/*<label htmlFor="email">Dejanos tu email</label>*/}
+        <input
+          type="email"
+          name="email"
+          placeholder="Dejanos tu email"
+          value={this.state.email}
+          onChange={this.onChange.bind(this)}
+          className={styles.input}></input>
+        <botton type="submit" name="boton" className={styles.enviar}></botton>
+      </form>
+    );
+  }
 }
-
-
-
-
-
-
 
 export default Form;
