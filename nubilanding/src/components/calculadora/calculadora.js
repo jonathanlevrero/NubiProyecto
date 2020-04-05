9b98e7c02a5420d6fc1a8786dbf197d162f97dc5
MatiiAutalan <autalan.matiash@gmail.com>
2020-04-04 22:35:12 -0300

    puse en gris los campos de recargas y dolares para que parescan inhabilitados, hice que en el campo de input text solo se puedan ingresar numeros, arregle un bug que tenia sobre donde clikear para seleccionar recargas y pesos

--- nubilanding/src/components/calculadora/calculadora.js
+++ nubilanding/src/components/calculadora/calculadora.js
@@ -3,24 +3,38 @@ import styles from "../calculadora/styles.module.scss";
 import Flecha from "../../assets/ic_calculator_arrow.svg";
 
 class Calculadora extends React.Component {
+  constructor() {
+    super();
+    this.state = { value: "" };
+    this.onChange = this.onChange.bind(this)
+  }
+ 
+  onChange(e) {
+    const regExp = /^[0-9\b]+$/;
+    if (e.target.value === "" || regExp.test(e.target.value)) {
+       this.setState({value: e.target.value})
+    } else {
+      alert("El campo solo acepta numeros")
+    }
+  }
   render() {
     return (
       <div className={styles.contenedor}>
         <h2 className={styles.titulo}>Calculá tu retiro o recarga</h2>
         <form className={styles.calculadora}>
           <div className={styles.seccionValor}>
-            <input type="text" maxLength="6" placeholder="0" name="monto" id="monto" />
+            <input type="text" maxLength="6" value={this.state.value} onChange={this.onChange}  placeholder="0" name="monto" id="monto" />
             <label className={styles.textoMontoInput}>
               Indicá el monto en Dólares
-            </label>
-            <label className={styles.inputRadio} htmlFor="recarga">
-              RECARGA
-              <input className={styles.inputCheck} type="radio" disabled="disabled" id="recarga" name="tipodeoperacion" value="recarga"/>
-              <span className={styles.marcado}/>
             </label>
             <label className={styles.inputRadio} htmlFor="retiro">
               RETIRO
               <input className={styles.inputCheck} type="radio" id="retiro" name="tipodeoperacion" value="retiro"/>
+              <span className={styles.marcado}/>
+            </label>
+            <label className={styles.inputRadio} htmlFor="recarga">
+              RECARGA
+              <input className={styles.inputCheck} type="radio" disabled="disabled" id="recarga" name="tipodeoperacion" value="recarga"/>
               <span className={styles.marcado}/>
             </label>
           </div>
@@ -28,7 +42,7 @@ class Calculadora extends React.Component {
             <img src={Flecha} alt="Boton para Calcular" />
           </div>
           <div className={styles.seccionValor}>
-            <input type="text" maxLength="6" placeholder="0" name="resultado" id="resultado"/>
+            <input className={styles.inhabilitado} type="text" maxLength="6" readOnly="readonly" placeholder="0" name="resultado" id="resultado"/>
             <label className={styles.textoMontoInput}>
               Monto estimado (Tarifa incluída)
             </label>
