dfafc95b777694c9a9942af64e3abfed81939372
MatiiAutalan <autalan.matiash@gmail.com>
2020-04-08 15:10:15 -0300

    Acomode los tags y clases en el archivo de estilos

--- nubilanding/src/components/terminos-condiciones/terminos-condiciones.js
+++ nubilanding/src/components/terminos-condiciones/terminos-condiciones.js
@@ -7,16 +7,14 @@ function TerminosCondiciones() {
       <div className={styles.contenedorTitulo}>
         <h1>Términos y Condiciones</h1>
       </div>
-      <div className={styles.contenedorTituloSecundario}>
-        <h2>
-          Los presentes términos y condiciones regulan tu relación con Nubi
-          respecto de las operaciones de retiro y recarga de tus fondos en tu
-          cuenta PayPal. Por favor lee bien las mismas antes de operar.
-        </h2>
-      </div>
+      <h2 className={styles.tituloSecundario}>
+        Los presentes términos y condiciones regulan tu relación con Nubi
+        respecto de las operaciones de retiro y recarga de tus fondos en tu
+        cuenta PayPal. Por favor lee bien las mismas antes de operar.
+      </h2>
       <div className={styles.contenedorInformacion}>
-        <h2 className={styles.tituloSeccion}>1) ¿Quiénes somos?</h2>
-        <p className={styles.texto}></p>
+        <h2>1) ¿Quiénes somos?</h2>
+        <p> holis</p>
       </div>
     </div>
   );
