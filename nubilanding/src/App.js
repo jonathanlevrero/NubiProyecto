a90c64fed131d6b99c9132fb6ed762aa723be56e
jenn-lang <jennifervallejos08@gmail.com>
2020-04-12 03:46:36 -0300

    cree las carpetas components, assents,empresas y los archivos form.js empresas.js styles.module.css; agregue en index.css el import de fonts,App.js agregue la tag Empresas y modifique las comillas,en empresas.js hice todos los contenedores y sus tag de p y img,importe las imagenes,en assets guarde imagenes y en form.js le agregue tags de formulario

--- nubilanding/src/App.js
+++ nubilanding/src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from "react";
+import Empresas from "./components/empresas/empresas"; /*temporal-hasta terminar todo el maquetado y me aprueben el pr*/
 
 function App() {
   return (
     <div>
-    
+      <Empresas />
     </div>
   );
 }
---
+++ nubilanding/src/components/empresas/form.js
@@ -0,0 +1,18 @@
+import React from "react";
+/*import styles from "./styles.module.css";*/
+
+class Form extends React.Component{
+    render() {
+        return(
+            <div>
+                <form>
+                    <label></label>
+                    <input></input>
+                    <button>Enviar</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Form;
